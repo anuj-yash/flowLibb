171e6ea8ae411a83cf9f062fd79029b4a2fda4db
anuj soni <anuj.soni@yash.com>
2025-02-27 10:21:08 +0530

    dfirst

---
+++ flow_dialog_lib/src/main/java/com/anuj/flow_dialog_lib/FlowListener.kt
@@ -0,0 +1,27 @@
+package com.anuj.flow_dialog_lib
+
+import android.app.Activity
+import android.app.Dialog
+import android.view.Window
+import android.widget.TextView
+
+class FlowListener {
+    fun showDialog(activity: Activity) {
+        val dialog = Dialog(activity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog)
+
+        val yesBtn = dialog.findViewById(R.id.tvCancle) as TextView
+        yesBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        val noBtn = dialog.findViewById(R.id.tvOk) as TextView
+        noBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+}
